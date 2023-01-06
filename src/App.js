98fa3ecd18cdd3b3d6832379ef7899f92591ceed
unknown <hasnainpriam925@gmail.com>
2023-01-06 11:33:45 +0600

    about blog contact error footer gallery added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
+import Error from './Pages/Error/Error';
+import Footer from './Pages/Footer/Footer';
+import Header from './Pages/Header/Header';
+// import ManageInventory from './Pages/Inventory/ManageInventory';
+// import UpdateProduct from './Pages/Inventory/updateProduct';
+// import AddItem from './Pages/Item/AddItem';
+// import MyItems from './Pages/Item/MyItems';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Navbar/Navbar';
+import Register from './Pages/Register/Register';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Header></Header>}></Route>
+        <Route path='/blog' element={
+          <Blog></Blog>
+        }></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        {/* <Route path='/manageItem' element={<RequireAuth><ManageInventory></ManageInventory></RequireAuth>}></Route> */}
+        {/* <Route path='/addItem' element={<RequireAuth><AddItem></AddItem></RequireAuth>}></Route> */}
+        {/* <Route path='/myItem' element={<RequireAuth><MyItems></MyItems></RequireAuth>}></Route> */}
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        {/* <Route path='/update/:id' element={<UpdateProduct></UpdateProduct>}></Route> */}
+
+        <Route path='*' element={<Error></Error>}></Route>
+
+      </Routes>
+      <Footer></Footer>
+
     </div>
   );
 }
--- src/Pages/Starts/Starts.js
+++ src/Pages/Starts/Starts.js
@@ -1,11 +1,46 @@
 import React from 'react';
 
 const Starts = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <>
+            <div className="bg-white py-6 sm:py-8 lg:py-12 lg:mt-20 lg:mb-32">
+                <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
+
+                    <div className="mb-10 md:mb-16">
+                        <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Our Sales by the numbers</h2>
+
+                        <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">Compustar Private Limited is the brainchild of union group representing BTRC approved iPhone in Bangladesh market. CPL is the authorized re-seller of iPhone and a proud National distributor of this elegant brand.</p>
+                    </div>
+
+
+                    <div className="grid grid-cols-2 md:grid-cols-4 md:divide-x gap-8 md:gap-0">
+
+                        <div className="flex flex-col items-center md:p-4">
+                            <div className="text-gray-400 text-xl sm:text-2xl md:text-3xl font-bold">20</div>
+                            <div className="text-sm sm:text-base font-semibold">Shops</div>
+                        </div>
+
+                        <div className="flex flex-col items-center md:p-4">
+                            <div className="text-gray-400 text-xl sm:text-2xl md:text-3xl font-bold">1000+</div>
+                            <div className="text-sm sm:text-base font-semibold">Customer</div>
+                        </div>
+
+
+                        <div className="flex flex-col items-center md:p-4">
+                            <div className="text-gray-400 text-xl sm:text-2xl md:text-3xl font-bold">4000+</div>
+                            <div className="text-sm sm:text-base font-semibold">Products</div>
+                        </div>
+
+                        <div className="flex flex-col items-center md:p-4">
+                            <div className="text-gray-400 text-xl sm:text-2xl md:text-3xl font-bold">A couple</div>
+                            <div className="text-sm sm:text-base font-semibold">Coffee breaks</div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Starts;
